817a91abd93f82a0f3f60ce26417b3874901fe6e
Tim Meermans <timmeermans@live.nl>
2018-10-17 16:18:34 +0200

    commit so I can review Dick Rich'em's code :)

---
+++ Assets/Scripts/Notification.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Notification : MonoBehaviour {
+    public string content;
+    //public [insert image type here] image;
+    //public Location2D location;
+    public string sender;
+    public bool relevance;
+    //public [insert time type here] time;
+    
+}
