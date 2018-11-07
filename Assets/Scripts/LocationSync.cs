8c9a93e08881598e4fe71a2f2bb6db0e9f2d471c
Bart <dangerbart@outlook.com>
2018-11-07 11:48:26 +0100

    Started creating representation for officer on 2D map

---
+++ Assets/Scripts/LocationSync.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationSync : MonoBehaviour {
+
+	public GameObject officer;
+	public GameObject officerMap;
+
+	private RectTransform officerMapTransform;
+
+	void Update () {
+		Vector2 vect = new Vector3(officer.transform.position.x, officer.transform.position.z, 0);
+		Debug.Log(officer.transform.position.x);
+		Debug.Log(officer.transform.position.z);
+
+		officerMap.GetComponent<RectTransform>().position = vect;
+	}
+}
