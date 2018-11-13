279e09cb93ad05bc300063e210de8a54762d5234
Tim Meermans <timmeermans@live.nl>
2018-11-13 16:19:29 +0100

    Added nearly working wandering pedestrian AI

--- Assets/Scripts/NPCBehaviour.cs
+++ Assets/Scripts/NPCBehaviour.cs
@@ -1,24 +1,87 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class NPCBehaviour : MonoBehaviour {
 
-	public GameObject checkpointContainer;
+    public GameObject checkpointContainer;
+    
+    private Node target = null;
+    private Node prevCheckpoint;
+    private Node[] list = new Node[8];
+    private NavMeshAgent agent = new NavMeshAgent();
 
-	private Vector3 spawnLocation;
+    // Use this for initialization
+    void Start() {
+        #region Checkpoint node initialization
+        /*
+        List goes by checkpoint index number in CheckpointContainer parent, so the GameObject named Checkpoint (3) is equal to node3 and GetCheckpointChild(3)
+        */
+        Node node0 = new Node(GetCheckpointChild(0));
+        Node node1 = new Node(GetCheckpointChild(1));
+        Node node2 = new Node(GetCheckpointChild(2));
+        Node node3 = new Node(GetCheckpointChild(3));
+        Node node4 = new Node(GetCheckpointChild(4));
+        Node node5 = new Node(GetCheckpointChild(5));
+        Node node6 = new Node(GetCheckpointChild(6));
+        Node node7 = new Node(GetCheckpointChild(7));
 
-	// Use this for initialization
-	void Start () {
-		spawnLocation = checkpointContainer.gameObject.transform.GetChild(Random.Range(0, checkpointContainer.transform.childCount - 1)).position;
-		Debug.Log("spawnLocation: " + spawnLocation);
-		Debug.Log("childCount: " + checkpointContainer.transform.childCount);
+        node0.SetOptions(new Node[] { node1 , node3, node6 });
+        node1.SetOptions(new Node[] { node0, node2, node7 });
+        node2.SetOptions(new Node[] { node1, node3 });
+        node3.SetOptions(new Node[] { node0, node2, node4 });
+        node4.SetOptions(new Node[] { node3, node5 });
+        node5.SetOptions(new Node[] { node5 });
+        node6.SetOptions(new Node[] { node0, node7 });
+        node7.SetOptions(new Node[] { node1, node6 });
 
-		this.transform.position = spawnLocation;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        list[0] = node0;
+        list[1] = node1;
+        list[2] = node2;
+        list[3] = node3;
+        list[4] = node4;
+        list[5] = node5;
+        list[6] = node6;
+        list[7] = node7;
+        #endregion
+        int random = Random.Range(0, list.Length - 1);
+        agent = GetComponent<NavMeshAgent>();
+
+        // Test
+        PrintDitto(list);
+
+        this.agent.Warp(list[random].GetTransformData().position);
+        prevCheckpoint = list[random];
+    }
+
+    // Update is called once per frame
+    void Update() {
+        int rand;
+        if(target == null) {
+            rand = Random.Range(0, prevCheckpoint.GetLength());
+            target = prevCheckpoint.GetOption(rand);
+            agent.SetDestination(target.GetTransformData().position);
+            FaceTarget();
+        } 
+        if(this.transform.position.x == target.GetTransformData().position.x && this.transform.position.z == target.GetTransformData().position.z) {
+            prevCheckpoint = target;
+            target = null;
+        }
+    }
+
+    private Transform GetCheckpointChild(int index) {
+        return checkpointContainer.gameObject.transform.GetChild(index);
+    }
+
+    public void PrintDitto(Node[] nodes) {
+        foreach (Node node in nodes) {
+            Debug.Log(node.GetTransformData().gameObject.name + " is connected to " + node.GetLength());
+        }
+    }
+
+    void FaceTarget() {
+        Vector3 direction = (target.GetTransformData().position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
 }
