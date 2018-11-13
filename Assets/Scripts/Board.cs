0ff38fb2b49819a92ae70f21db30034bfd018246
Richard Mastenbroek <rmastenbroek18@gmail.com>
2018-11-13 15:31:38 +0100

    Added XML files to Unity

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -7,15 +7,6 @@ public class Board :  MonoBehaviour {
     public GameObject notificationbar;
 
     public void createNotification() {
-
-    }
-
-    public void exampleNotification() {
-        Notification example = new Notification(notificationbar);
-        example.content = "0w0 what's this? A notifcation for meee??? >w<";
-        example.sender = "cringy weeb";
-        example.relevance = true;
-
-        example.add();
+        Notification notification = new Notification(notificationbar, "SomeMessage", true);
     }
 }
--- Assets/Scripts/Notification.cs
+++ Assets/Scripts/Notification.cs
@@ -3,30 +3,43 @@ using UnityEngine.UI;
 
 public class Notification : MonoBehaviour {
     //Set up variables needed for notifications
-    public string content;
-    public Image image;
-    //public Location2D location;
-    public string sender;
-    public bool relevance;
-    //public [insert time type here] time;
-    public Image platform;
+    static System.Random rnd = new System.Random();
+    private string sender;
+    private string message;
+    private bool relevance;
     private GameObject container;
     private RectTransform rect;
     private CanvasRenderer canvasRen;
-    private Image img;
+
     private static float dynamicX = -664;
     private float dynamicY = -455;
     private float notificationWidth = 100;
     private float notificationHeight = 31;
 
-    public Notification(GameObject notificationbar) {
+    public Notification(GameObject notificationbar, string message, bool relevance)
+    {
+        CreateShape(notificationbar);
+        this.sender = CreateSender();
+        Debug.Log(this.sender);
+        this.message = message;
+        this.relevance = relevance;
+        add();
+    }
+    private string CreateSender()
+    {
+        int offset = rnd.Next(0,5);
+        int firstnameID = rnd.Next(rnd.Next(0,offset), rnd.Next(firstname.Length-offset,firstname.Length));
+        int lastnameID = rnd.Next(rnd.Next(0,offset), rnd.Next(lastname.Length-offset,lastname.Length));
+        return ((string)firstname[firstnameID] + " " + (string)lastname[lastnameID]);
+    }
+
+    private void CreateShape(GameObject notificationbar)
+    {
         container = new GameObject("NotificationContainer");
         container.transform.SetParent(notificationbar.transform);
         rect = container.AddComponent<RectTransform>();
         canvasRen = container.AddComponent<CanvasRenderer>();
-        img = container.AddComponent<Image>();
     }
-
     public void add() {
         rect.transform.localPosition = new Vector2(dynamicX - notificationWidth, dynamicY);
         dynamicX += 540;      
