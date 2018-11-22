d006ab0a0b94bbe1266b114dea73b035c8b77fca
Andono <jordyberg@gmail.com>
2018-11-22 11:15:36 +0100

    Merged Rishard's code with mine

--- Assets/Scripts/Notification.cs
+++ Assets/Scripts/Notification.cs
@@ -9,33 +9,4 @@ public class Notification{
     public string Bericht;
     public int POI;
     public string Mediaplatform;
-
-
-    ////Set up shape for notifications
-    //private GameObject container;
-    //private RectTransform rect;
-    //private CanvasRenderer canvasRen;
-
-    ////Set up shape for notifications
-    //private static float dynamicX = -664;
-    //private float dynamicY = -455;
-    //private float notificationWidth = 100;
-    //private float notificationHeight = 31;
-
-    //public Notification(GameObject notificationbar, string message, bool relevance)
-    //{
-    //}
-
-    //private void CreateShape(GameObject notificationbar)
-    //{
-    //    container = new GameObject("NotificationContainer");
-    //    container.transform.SetParent(notificationbar.transform);
-    //    rect = container.AddComponent<RectTransform>();
-    //    canvasRen = container.AddComponent<CanvasRenderer>();
-    //}
-    //public void add() {
-    //    rect.transform.localPosition = new Vector2(dynamicX - notificationWidth, dynamicY);
-    //    dynamicX += 540;      
-    //    rect.sizeDelta = new Vector2(notificationWidth, notificationHeight);
-    //}
 }
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public GameObject panelToShowNotifications;
+    public float periodInSeconds = 10f;
+    private float nextActionTime = 0.0f;
+
+    // Social media creator
+    public GameObject socialMediaPrefab;
+    GameObject socialMediaPrefabClone;
+
+
+    // Test
+    private int counter = 0;
+
+    // Use this for initialization
+    void Start () {
+        socialMediaPrefabClone = Instantiate(socialMediaPrefab);
+	}
+
+   
+
+    void Update()
+    {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += periodInSeconds;
+
+            // execute block of code here
+            counter++;
+            Debug.Log("Get messages to show on screen: " + counter);
+
+            //Turned off for now
+            //CreateMessage("Hello word", Color.blue);
+        }
+    }
+
+    public void CreateMessage(string text, Color color)
+    {
+        GameObject newCanvas = new GameObject("Canvas");
+        Canvas c = newCanvas.AddComponent<Canvas>();
+        c.renderMode = RenderMode.ScreenSpaceOverlay;
+        newCanvas.AddComponent<CanvasScaler>();
+        newCanvas.AddComponent<GraphicRaycaster>();
+        GameObject panel = new GameObject("Panel");
+        panel.AddComponent<CanvasRenderer>();
+        Image i = panel.AddComponent<Image>();
+        i.color = Color.red;
+        panel.transform.SetParent(panelToShowNotifications.transform, false);
+
+    }
+
+    public void AddMessageToPanel(){
+
+    }
+
+}
