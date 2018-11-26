4bfb5970dd3eb27f41652366c81a568586fe93e4
Andono <jordyberg@gmail.com>
2018-11-26 09:54:11 +0100

    Grid list for messages works now

---
+++ Assets/Scripts/Inventory/NotificationButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NotificationButton : MonoBehaviour {
+
+    [SerializeField]
+    private Text name;
+
+    [SerializeField]
+    private Text message;
+
+    public void SetName(string name)
+    {
+        this.name.text = name;
+    }
+}
---
+++ Assets/Scripts/Inventory/NotificationControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationControl : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject MessagePanel;
+
+    void Start()
+    {
+        for (int i = 0; i < 1; i++){
+            CreateMessagePanel(i);
+        }
+    }
+
+    void CreateMessagePanel(int number){
+        GameObject message = Instantiate(MessagePanel) as GameObject;
+        message.SetActive(true);
+
+        message.GetComponent<NotificationButton>().SetName("User " + number);
+
+        message.transform.SetParent(MessagePanel.transform.parent, false);
+    }
+}
