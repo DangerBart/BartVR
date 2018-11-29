b800e8eadedd5a6e5d1168fa037501e28089b105
Richard Mastenbroek <rmastenbroek18@gmail.com>
2018-11-29 10:27:00 +0100

    Cleaned up code

---
+++ Assets/Dev/Scripts/NPC's/NPCManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCManager : MonoBehaviour {
+
+    public GameObject NPC;
+    public GameObject CheckpointContainer;
+
+    [Tooltip("Amount of NPC's to spawn")]
+    public int amount;
+
+    private NPCBehaviour npcBehaviour;
+
+    // Use this for initialization
+    void Start () {
+        for (int i = 0; i < amount; i++) {
+            CreateNPC(this.gameObject);
+        }
+	}
+
+    private void CreateNPC(GameObject container) {
+        GameObject npc = Instantiate(NPC) as GameObject;
+        npcBehaviour = npc.GetComponent<NPCBehaviour>();
+        npcBehaviour.checkpointContainer = CheckpointContainer; 
+        npc.SetActive(true);
+        npc.transform.SetParent(container.transform);
+    }
+}
