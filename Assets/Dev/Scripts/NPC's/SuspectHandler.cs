274f21fe197aaf360617cda5004f9448775101c2
Bart <dangerbart@outlook.com>
2018-12-03 13:33:14 +0100

    Fixed text

--- Assets/Dev/Scripts/NPC's/SuspectHandler.cs
+++ Assets/Dev/Scripts/NPC's/SuspectHandler.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class SuspectHandler : MonoBehaviour {
     public GameObject player;
-    public GameObject gameOverCanvas;
+    public GameObject gameOverText;
 
-    private float diameter;
+    private float radius;
     private Transform arrestSphere;
 
     // Use this for initialization
     void Start () {
         arrestSphere = gameObject.transform.GetChild(1);
-        diameter = arrestSphere.lossyScale.y / 4;
+        radius = arrestSphere.lossyScale.y / 4;
     }
 	
 	// Update is called once per frame
 	void Update () {
         //If player is within suspect's arresting radius
-		if(player.transform.position.x >= this.transform.position.x || player.transform.position.x <= this.transform.position.x ||
-           player.transform.position.z >= this.transform.position.z || player.transform.position.z <= this.transform.position.z) {
-            gameOverCanvas.SetActive(true);
+		if((player.transform.position.x >= this.transform.position.x + radius || player.transform.position.x <= this.transform.position.x - radius) &&
+           (player.transform.position.z >= this.transform.position.z + radius || player.transform.position.z <= this.transform.position.z - radius)) {
+            gameOverText.SetActive(true);
             Time.timeScale = 0;
+            Debug.Log("Player x: " + player.transform.position.x);
+            Debug.Log("Player z: " + player.transform.position.z);
+            Debug.Log("Suspect x: " + this.transform.position.x);
+            Debug.Log("Suspect z: " + this.transform.position.z);
         }
 	}
 }
