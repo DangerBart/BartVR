42eb9f034ec3e0d01f28413b282870a991dce988
Tim Meermans <timmeermans@live.nl>
2018-12-03 12:49:54 +0100

    Added .meta files for fix

---
+++ Assets/Dev/Scripts/NPC's/SuspectHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuspectHandler : MonoBehaviour {
+    public GameObject player;
+    public GameObject gameOverCanvas;
+
+    private float diameter;
+    private Transform arrestSphere;
+
+    // Use this for initialization
+    void Start () {
+        arrestSphere = gameObject.transform.GetChild(1);
+        diameter = arrestSphere.lossyScale.y / 4;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //If player is within suspect's arresting radius
+		if(player.transform.position.x >= this.transform.position.x || player.transform.position.x <= this.transform.position.x ||
+           player.transform.position.z >= this.transform.position.z || player.transform.position.z <= this.transform.position.z) {
+            gameOverCanvas.SetActive(true);
+            Time.timeScale = 0;
+        }
+	}
+}
