dbd202a4edaf79fe451200e8dab1a21856411633
Tim Meermans <timmeermans@live.nl>
2018-12-04 10:38:34 +0100

    Removed suspecthandler script and added arresthandler script to use collisions instead of position checks

---
+++ Assets/Dev/Scripts/NPC's/ArrestHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrestHandler : MonoBehaviour {
+
+    public GameObject gameOverText;
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.gameObject.tag == "Arrest") {
+            Time.timeScale = 0;
+            gameOverText.SetActive(true);
+        }
+    }
+}
