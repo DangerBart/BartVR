bce2e4460b64c7689b754c88e2a559f4d4145ac7
Bart <dangerbart@outlook.com>
2018-12-05 13:35:24 +0100

    POI system works

--- Assets/Dev/Scripts/Control Room/Board.cs
+++ Assets/Dev/Scripts/Control Room/Board.cs
@@ -13,7 +13,7 @@ public class Board : MonoBehaviour
 
     private NotificationControl notificationControl;
 
-    private NotificationContainer nc;
+    private NotificationContainer notificationContainer;
     private int irrelevantNotificationCount;
     public string m_Path = "XML_Files/data-set";
     Dictionary<int, List<Notification>> notificationsPerPOI = new Dictionary<int, List<Notification>>();
@@ -22,25 +22,18 @@ public class Board : MonoBehaviour
         LoadItems(m_Path);
         FillDictionaryWithNotificationsPerPOI();
         notificationControl = notificationMenu.GetComponent<NotificationControl>();
-
-        // POI
         POIManager = POISystem.GetComponent<POIManager>();
-        // POI 0 is not a real POI
+
+        // Count -1 as we don't need a POI on the map for irrelevant messages
         POIManager.Setup(notificationsPerPOI.Count - 1);
     }
 
-    public void PlayerMoved(Transform playerCoordinates)
-    {
-        Debug.Log("Player moved -> We should check if the player is near the current POI. These are the player cordinates: X " + playerCoordinates.transform.position.x + " Y " + playerCoordinates.transform.position.y);
-    }
-
     void LoadItems(string path) {
-        nc = NotificationContainer.Load(path);
+        notificationContainer = NotificationContainer.Load(path);
     }
-
 
     void FillDictionaryWithNotificationsPerPOI() {
-        foreach (Notification note in nc.notifications) {
+        foreach (Notification note in notificationContainer.notifications) {
 
             if (!notificationsPerPOI.ContainsKey(note.POI)){
                 notificationsPerPOI.Add(note.POI, new List<Notification>());
@@ -67,7 +60,7 @@ public class Board : MonoBehaviour
 
     public void LoadRandomIrrelevantNotification() {
         // Reset counter if needed
-        if (irrelevantNotificationCount >= notificationsPerPOI[0].Count){
+        if (irrelevantNotificationCount >= notificationsPerPOI[0].Count) {
             irrelevantNotificationCount = 0;
         }
 
--- Assets/Dev/Scripts/POI/POIManager.cs
+++ Assets/Dev/Scripts/POI/POIManager.cs
@@ -1,36 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 public class POIManager : MonoBehaviour {
 
     private int currentPOI;
-    private int amountOfPOI;
 
     private List<GameObject> POIs = new List<GameObject>();
 
     public void Setup(int amountOfPOI) {
         currentPOI = 1;
-        this.amountOfPOI = amountOfPOI;
         FillPOIList(amountOfPOI);
-        PrintAllPOI();
     }
 
     public int GetCurrentPOI() {
         return currentPOI;
     }
 
+    public Transform GetPOITransformData() {
+        return POIs[currentPOI - 1].transform;
+    }
+
     public void POIReached() {
-        if (currentPOI <= POIs.Count) {
+        if (currentPOI < POIs.Count) {
             currentPOI++;
             EnableCollider(POIs[currentPOI - 1]);
         }
-        else
-        {
-            Debug.Log("Last POI reached");
-        }
-
-        Debug.Log("Current POI is now: " + currentPOI);
     }
 
     private void FillPOIList(int amount) {
@@ -41,27 +35,15 @@ public class POIManager : MonoBehaviour {
                 EnableCollider(POIs[i], false);
             }
         }
-
-        Debug.Log("Done with filling the POI list");
     }
 
+    // Retrieve POI's from map
     private GameObject GetPOIChild(int index) {
         return gameObject.transform.GetChild(index).gameObject;
     }
 
-    private void EnableCollider(GameObject poiObject, bool value = true)
-    {
+    private void EnableCollider(GameObject poiObject, bool value = true) {
         poiObject.GetComponent<SphereCollider>().enabled = value;
-    }
-
-
-    // TEST
-    public void PrintAllPOI()
-    {
-        foreach (GameObject POI in POIs)
-        {
-            Debug.Log(POI.transform.position.x);
-        }
     }
 
 }
