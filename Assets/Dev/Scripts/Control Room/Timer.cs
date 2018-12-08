637ff60e35a9b7183f094cc35b5b4a8f551e59c7
Richard Mastenbroek <rmastenbroek18@gmail.com>
2018-12-08 21:10:23 +0100

    Time and Date work now and cleaned up code

--- Assets/Dev/Scripts/Control Room/Timer.cs
+++ Assets/Dev/Scripts/Control Room/Timer.cs
@@ -2,11 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class Timer : MonoBehaviour {
-
-    [SerializeField]
-    public GameObject panelToShowNotifications;
+    [Tooltip("Update in game time")]
+    public float updateTimeEvery;
 
     [Tooltip("Time in seconds")]
     [SerializeField]
@@ -19,27 +17,50 @@ public class Timer : MonoBehaviour {
 
     // Social media creator
     private Board board;
+    public GameObject timestamp;
+    private Text currentDate;
+    private Text currentTime;
+    private int hour;
+    private double minute;
 
     void Start () {
         board = GetComponent<Board>();
-	} 
+        currentDate = timestamp.transform.Find("Date").GetComponent<Text>();
+        currentTime = timestamp.transform.Find("Time").GetComponent<Text>();
+        currentDate.text = "Date: " + System.DateTime.Now.ToString("dd/MM/yyyy");
+        LoadTime();
+        InvokeRepeating("UpdateGameTime",0,updateTimeEvery);
+    } 
 
     void Update()
     {
         // Relevant notifications
-        if (Time.time > relevantActionTime)
-        {
+        if (Time.time > relevantActionTime){
             relevantActionTime += intervalRelevantMessages;
             board.LoadRandomRelevantNotification();
         }
 
         // irrelevant notifications
-        if (Time.time > irrelevantActionTime)
-        {
+        if (Time.time > irrelevantActionTime){
             irrelevantActionTime += intervalIrrelevantMessages;
             board.LoadRandomIrrelevantNotification();
         }
-
     }
-
+    private void LoadTime(){
+        var date = System.DateTime.Now;
+        hour = date.Hour;
+        minute = date.Minute;
+    }
+    private void UpdateGameTime(){
+        minute += 1;
+        if(minute >= 60){
+            minute = 0;
+            hour += 1;
+            if(hour >= 24){
+                hour = 0;
+            }
+        }
+            
+        currentTime.text = string.Format("Time: {0}:{1}", hour.ToString("00"), minute.ToString("00"));
+    }
 }
