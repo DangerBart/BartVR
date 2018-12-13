002ecb80ace3d9974bac3a1de62807b31522185f
Bart <dangerbart@outlook.com>
2018-12-13 11:14:38 +0100

    Notifications now appear as they are supposed to also cleaned up code

--- Assets/Dev/Scripts/Control Room/NotificationTimer.cs
+++ Assets/Dev/Scripts/Control Room/NotificationTimer.cs
@@ -11,27 +11,18 @@ public class NotificationTimer : MonoBehaviour {
     [Tooltip("Time in seconds")]
     public float intervalIrrelevantMessages = 2f;
 
-    private bool isActive = false;
-    private bool doOnce = true;
-
-    //private void Awake() {
-
-    //}
-
     private void OnEnable() {
         SceneManager.sceneLoaded += OnSceneLoaded;
     }
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
-        Invoke("Setup", 0);
+        //Repeatedly call the addnotification function with an interval
+        InvokeRepeating("AddRelevantNotification", 0, intervalRelevantMessages);
+        InvokeRepeating("AddIrrelevantNotification", 0, intervalIrrelevantMessages);
     }
 
     private void OnDisable() {
         SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-    public void Setup() {
-        InvokeRepeating("AddRelevantNotification", 0, intervalRelevantMessages);
-        InvokeRepeating("AddIrrelevantNotification", 0, intervalIrrelevantMessages);
     }
 
     void AddRelevantNotification() {
--- Assets/Dev/Scripts/Managers/Gamemanager.cs
+++ Assets/Dev/Scripts/Managers/Gamemanager.cs
@@ -19,6 +19,8 @@ public class Gamemanager : MonoBehaviour {
 	public void StartGame() {
         if (amountOfNpcs.text != "" && amountOfNpcs.text != "-" && amountOfNpcsToSpawn > 0 && amountOfNpcsToSpawn <= 250) {
             SceneManager.LoadScene(1);
+            //Start time
+            Time.timeScale = 1;
         } else {
             inputRequired.GetComponent<Text>().text = "Vul een geldig tussen 0 en 250 getal in";
             inputRequired.SetActive(true);
