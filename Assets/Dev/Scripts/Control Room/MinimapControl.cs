92a16f338328aa25932e38b93443cfef5aef9750
Bart <dangerbart@outlook.com>
2018-12-20 12:30:38 +0100

    Second playtest and small fixes

--- Assets/Dev/Scripts/Control Room/MinimapControl.cs
+++ Assets/Dev/Scripts/Control Room/MinimapControl.cs
@@ -7,7 +7,7 @@
         if (notification.POI == 0) {             SetIrrelevantNotificationLocation(notification);         } else {             SetRelevantNotificationLocation(notification);         }     }      private void SetIrrelevantNotificationLocation(Notification notification) {         float corX = Random.Range(minRangeX, maxRangeX);         float corY = Random.Range(minRangeY, maxRangeY);         notification.MinimapLocation = new Vector2(corX, corY);
     }      private void SetRelevantNotificationLocation(Notification notification) {
         // Calculate location of POI relatively to minimap
-        Vector2 POILocation = new Vector2(-1 * (notification.POILocation.x * xScale), (-1 * (notification.POILocation.z * yScale)) + defaultMarker.GetComponent<RectTransform>().rect.height);         notification.MinimapLocation = POILocation + Random.insideUnitCircle * 60;     }      public void CreateNewMarker(Vector2 minimapLocation, bool selectedMarker = false) {
+        Vector2 POILocation = new Vector2(-1 * (notification.POILocation.x * xScale) + defaultMarker.GetComponent<RectTransform>().rect.width / 2, (-1 * (notification.POILocation.z * yScale)) + defaultMarker.GetComponent<RectTransform>().rect.height);         notification.MinimapLocation = POILocation + Random.insideUnitCircle * 60;     }      public void CreateNewMarker(Vector2 minimapLocation, bool selectedMarker = false) {
         GameObject marker = Instantiate(defaultMarker) as GameObject;          if (selectedMarker) {
             marker.GetComponent<Image>().sprite = Resources.Load<Sprite>("Notification/location-pointer-yellow");
         }          marker.SetActive(true);         marker.transform.SetParent(this.transform, false);          // Set marker on correct location         marker.transform.localPosition = minimapLocation;
@@ -19,7 +19,8 @@
     }      private void Setup() {
         CalculateScale();         CalculateBoundries();
     }      private void CalculateScale() {
-        float offset = 2f;          Vector2 mapSize = minimap.GetComponent<RectTransform>().sizeDelta;         Vector2 planeSize = plane.GetComponent<RectTransform>().sizeDelta;         xScale = planeSize.x / mapSize.x + offset;         yScale = planeSize.y / mapSize.y + offset;
+        float offsetX = 1.7f;
+        float offsetY = 2f;          Vector2 mapSize = minimap.GetComponent<RectTransform>().sizeDelta;         Vector2 planeSize = plane.GetComponent<RectTransform>().sizeDelta;         xScale = planeSize.x / mapSize.x + offsetX;         yScale = planeSize.y / mapSize.y + offsetY;
     }      private void CalculateBoundries() {
         float saveMargin = 100f; 
         minRangeX = (minimap.GetComponent<RectTransform>().sizeDelta.x / 2 * -1) + saveMargin;         maxRangeX = (minimap.GetComponent<RectTransform>().sizeDelta.x / 2) - saveMargin;         minRangeY = (minimap.GetComponent<RectTransform>().sizeDelta.y / 2 * -1) + saveMargin;         maxRangeY = (minimap.GetComponent<RectTransform>().sizeDelta.y / 2) - saveMargin;
