47ddf73e27e345c3fcd24ecad26f3a399e893b0c
038Gamez <38555890+038Gamez@users.noreply.github.com>
2019-02-21 14:55:37 +0100

    Added custom laser pointer that interacts with UI, Fixed canvas clipping issue by changing UpdatePoses script. Added tag for laser pointer to recognize VR UI elements

---
+++ Assets/Dev/Scripts/VR/UILaser.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UILaser : MonoBehaviour {
+
+    private bool singlePress = false;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SteamVR_TrackedController controller = GetComponent<SteamVR_TrackedController>();
+
+        if (controller.triggerPressed && !singlePress) {
+            singlePress = true;
+            Ray raycast = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+            if (Physics.Raycast(transform.position, fwd, out hit)) {
+                Debug.Log("Hit : " + hit.collider.tag);
+                Debug.DrawRay(transform.position, fwd * hit.distance, Color.yellow);
+                if (hit.collider.tag == "VRUI") {
+                    Debug.Log("Name: " + hit.collider.name);
+                    hit.collider.gameObject.GetComponent<VRButtonInteractTest>().buttonTest();
+                }
+            }
+            singlePress = false;
+        }
+    }
+}
