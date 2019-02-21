508064ba71b75ea9eecb6a9f564409508b789d65
Tim Meermans <timmeermans@live.nl>
2019-02-21 10:55:01 +0100

    Added more tests for buttons and alerts

--- Assets/Dev/Scripts/VR/VRButtonInteractTest.cs
+++ Assets/Dev/Scripts/VR/VRButtonInteractTest.cs
@@ -1,8 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class VRButtonInteractTest : MonoBehaviour {
+    [SerializeField]
+    private GameObject notification;
+    [SerializeField]
+    private GameObject validate;
 
 	public void buttonTest() {
+        validate.SetActive(!validate.activeInHierarchy);
         Debug.Log("I have been clicked");
+    }
+
+    public void notificationTest() {
+        notification.SetActive(true);
     }
 }
