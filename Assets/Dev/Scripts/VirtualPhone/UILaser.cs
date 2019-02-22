a4c7d951eab64da20a41d2a6e35390cc4b7f5a83
038Gamez <38555890+038Gamez@users.noreply.github.com>
2019-02-22 11:33:58 +0100

    Cleaned up script directory a bit, commented and cleaned up code, got virtual phone to now support a map with it's own real-time location tracking just like on the control room screen

---
+++ Assets/Dev/Scripts/VirtualPhone/UILaser.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILaser : MonoBehaviour {
+    
+    private SteamVR_TrackedObject trackedObject;
+    private SteamVR_Controller.Device device;
+
+    // Use this for initialization
+    void Start () {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Get reference to controller through SteamVR_TrackedObject, 
+        //rather than SteamVR_TrackedController for access to more functions
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+
+        //Check if the trigger was pressed
+        if (device.GetHairTriggerDown()) {
+            //Send out a ray in the forward direction
+            Ray raycast = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+            //Check what the ray hit
+            if (Physics.Raycast(transform.position, fwd, out hit)) {
+                //If ray hit a button trigger the button's function
+                if (hit.collider.tag == "VRUIButton") {
+                    hit.collider.gameObject.GetComponent<Button>().onClick.Invoke();
+
+                } 
+            }
+        }
+    }
+}
---
+++ Assets/Dev/Scripts/VirtualPhone/VRSync.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRSync : MonoBehaviour {
+    //References
+    [SerializeField]
+    private GameObject map;
+    [SerializeField]
+    private GameObject plane;
+    [SerializeField]
+    private GameObject cameraRig;
+    //Variables for storing values during calculations
+    private Vector2 mapSize;
+    private Vector2 planeSize;
+    private float xScale;
+    private float yScale;
+    //Offset for more precise location to give to the icon
+    [SerializeField]
+    private float offsetx = 1.7f;
+    [SerializeField]
+    private float offsety = 2f;
+
+    // Use this for initialization
+    void Start () {
+        mapSize = getSize(map);
+        planeSize = getSize(plane);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        xScale = scale(planeSize.x, mapSize.x, offsetx);
+        yScale = scale(planeSize.y, mapSize.y, offsety);
+        //Set icon to location of the cameraRig
+        GetComponent<RectTransform>().transform.localPosition = new Vector2(-1 * (cameraRig.transform.position.x * xScale),
+                                                                                 -1 * (cameraRig.transform.position.z * yScale));
+    }
+
+    Vector2 getSize(GameObject go) {
+        return go.GetComponent<RectTransform>().sizeDelta;
+    }
+
+    float scale(float planeSize, float mapSize, float offset) {
+        return planeSize / mapSize + offset;
+    }
+}
