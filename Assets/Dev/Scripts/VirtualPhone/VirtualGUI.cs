58387ac4703113703ad83aab69f0f25d8dc680c6
038Gamez <38555890+038Gamez@users.noreply.github.com>
2019-03-05 13:28:00 +0100

    removed deprecated scripts and set everything to work in one script

--- Assets/Dev/Scripts/VirtualPhone/VirtualGUI.cs
+++ Assets/Dev/Scripts/VirtualPhone/VirtualGUI.cs
@@ -8,8 +8,8 @@ public class VirtualGUI : MonoBehaviour {
     private SteamVR_TrackedObject trackedObject;
     private SteamVR_Controller.Device device;
     //ButtonList
-    [Header("Add app panels in order from camera, map, menu")]
-    [Tooltip("Add app panels in order from camera, map, menu")]
+    [Header("Add app panels in order from top to bottom in hierarchy")]
+    [Tooltip("Add app panels in order from top to bottom in hierarchy")]
     [SerializeField]
     private List<GameObject> apps = new List<GameObject>();
     //MenuButtons
@@ -17,12 +17,24 @@ public class VirtualGUI : MonoBehaviour {
     [Tooltip("Add app buttons in order from left, top, right, bottom then deselect")]
     [SerializeField]
     private List<GameObject> menuApps = new List<GameObject>();
+    //CameraButtons
+    [Header("Add app buttons in order of takepicture, back, deselect")]
+    [Tooltip("Add app buttons in order of takepicture, back, deselect")]
+    [SerializeField]
+    private List<GameObject> cameraButton = new List<GameObject>();
+    //mapButtons
+    [Header("Add app buttons in order of enlargemap, back, deselect")]
+    [Tooltip("Add app buttons in order of enlargemap, back, deselect")]
+    [SerializeField]
+    private List<GameObject> mapButton = new List<GameObject>();
     //Enums
     private enum App {
-        camera = 0,
-        map = 1,
-        menu = 2,
-        none = 3
+        test = 0,
+        camera = 1,
+        app3 = 2,
+        map = 3,
+        menu = 4,
+        none = 5
     }
 
     private enum Direction {
@@ -33,6 +45,9 @@ public class VirtualGUI : MonoBehaviour {
         standby = 4
     }
 
+    //Logic variables
+    private float touchpadMargin = 0.60f;
+
     // Use this for initialization
     void Start() {
         trackedObject = GetComponentInParent<SteamVR_TrackedObject>();
@@ -41,7 +56,6 @@ public class VirtualGUI : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         device = SteamVR_Controller.Input((int)trackedObject.index);
-        Debug.Log("Index: " + (int)trackedObject.index);
 
         switch (CurrentApp()) {
             case App.camera:
@@ -53,8 +67,15 @@ public class VirtualGUI : MonoBehaviour {
             case App.menu:
                 RunMenu();
                 break;
+            case App.test:
+                //RunTest();
+                break;
+            case App.app3:
+                //RunApp3
+                break;
             default:
                 //Error: no app active
+                Debug.LogError("NO ACTIVE APP ON GUI");
                 break;
         }
     }
@@ -69,26 +90,72 @@ public class VirtualGUI : MonoBehaviour {
     }
 
     private void RunCamera() {
-
+        switch (TouchpadDirection(device)) {
+            case Direction.left:
+                cameraButton[0].GetComponent<Button>().Select();
+                break;
+            case Direction.right:
+                cameraButton[1].GetComponent<Button>().Select();
+                break;
+            case Direction.standby:
+                cameraButton[2].GetComponent<Button>().Select();
+                break;
+        }
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad)) {
+            if(TouchpadDirection(device) == Direction.left) {
+                TakePicture();
+            } else if (TouchpadDirection(device) == Direction.right) {
+                //return to main menu
+                apps[(int)App.camera].SetActive(false);
+                apps[(int)App.menu].SetActive(true);
+            }
+        }
     }
 
     private void RunMap() {
-
+        switch (TouchpadDirection(device)) {
+            case Direction.up:
+                mapButton[0].GetComponent<Button>().Select();
+                break;
+            case Direction.down:
+                mapButton[1].GetComponent<Button>().Select();
+                break;
+            case Direction.standby:
+                mapButton[2].GetComponent<Button>().Select();
+                break;
+        }
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad)) {
+            if (TouchpadDirection(device) == Direction.up) {
+                EnlargeMap();
+            } else if (TouchpadDirection(device) == Direction.down) {
+                //return to main menu
+                apps[(int)App.map].SetActive(false);
+                apps[(int)App.menu].SetActive(true);
+            }
+        }
     }
 
     private void RunMenu() {
         HighlightSelectedApp();
         //App was selected
-        if (device.GetPress(SteamVR_Controller.ButtonMask.Touchpad) && TouchpadDirection(device) != Direction.standby) {
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad) && TouchpadDirection(device) != Direction.standby) {
             LaunchApp((int)TouchpadDirection(device));
         }
     }
 
-    void LaunchApp(int app) {
+    private void LaunchApp(int app) {
         //Set main menu false
         apps[(int)App.menu].SetActive(false);
         //set selected app true
         apps[app].SetActive(true);
+    }
+
+    private void TakePicture() {
+        Debug.Log("HA");
+    }
+
+    private void EnlargeMap() {
+        Debug.Log("HA");
     }
 
     private void HighlightSelectedApp() {
@@ -119,19 +186,19 @@ public class VirtualGUI : MonoBehaviour {
         float touchpadX = device.GetAxis().x;
 
         //Player's finger is in the middle of the touchpad
-        if (touchpadY <= 0.75f && touchpadY >= -0.75f) {
+        if (touchpadY <= touchpadMargin && touchpadY >= -touchpadMargin) {
             //Player's finger is on the right side of the touchpad
-            if (touchpadX > 0.75f) {
+            if (touchpadX > touchpadMargin) {
                 return Direction.right;
-            } else if (touchpadX < -0.75f) {
+            } else if (touchpadX < -touchpadMargin) {
                 //Player's finger is on the left side of the touchpad
                 return Direction.left;
             }
         } else {
             //Player's hand is on the top side of the touchpad
-            if (touchpadY >= 0.75f) {
+            if (touchpadY >= touchpadMargin) {
                 return Direction.up;
-            } else if (touchpadY <= -0.75f) {
+            } else if (touchpadY <= -touchpadMargin) {
                 //Player's finger is on the bottom side of the touchpad
                 return Direction.down;
             }
