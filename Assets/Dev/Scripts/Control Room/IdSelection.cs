19ee69fce9829df9f122341bb35d72251317576b
unknown <jordyberg@gmail.com>
2019-04-05 11:19:40 +0200

    Added beginning of Idscript

---
+++ Assets/Dev/Scripts/Control Room/IdSelection.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IdSelection : MonoBehaviour {
+    // Needs to set in inspector
+    public Image genderImageToChange;
+    public NamedGenderImage[] GenderOptions;
+
+    [Serializable]
+    public struct NamedGenderImage
+    {
+        public Genders value;
+        public Texture2D image;
+    }
+
+    [Serializable]
+    public struct NamedColorImage
+    {
+        public Color value;
+        public Texture2D image;
+    }
+
+    // Use this for initialization
+    void Start () {
+     
+
+    }
+
+    public void LeftSelection()
+    {
+
+    }
+
+    public void RightSelection()
+    {
+
+    }
+}
