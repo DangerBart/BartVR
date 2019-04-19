0eb86b1725a760b6c28f102d8596cda6f57a4eef
unknown <jordyberg@gmail.com>
2019-04-19 17:13:35 +0200

    Made new model for combining messages

---
+++ Assets/Dev/Scripts/Models/MainNotification.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainNotification : MonoBehaviour {
+
+    public string keyNote;
+    public DateTime timeLatestNotification;
+    public Vector2 MinimapLocation;
+    List<Notification> notifications;
+}
