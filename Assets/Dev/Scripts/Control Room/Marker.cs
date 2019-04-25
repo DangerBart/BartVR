7fa59c2ab6bad1abbe84804c53790e985512e37d
jordy1111100 <jordyberg@gmail.com>
2019-04-25 16:10:01 +0200

    Marker changes upon mouse entering

---
+++ Assets/Dev/Scripts/Control Room/Marker.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Marker : MonoBehaviour{
+
+    #region Variables
+
+    private MainNotification MainNotif;
+
+    #endregion
+
+    // Use this for initialization
+    void Start () {
+        MainNotif = GetComponent<MainNotification>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    #region Public Functions
+    public void OnMouseClick()
+    {
+        // Detected click on panel
+        Debug.Log("Detected panel click");
+    }
+
+    public void OnMouseEnter()
+    {
+        Debug.Log("Mouse Enter");
+        SetActiveMarkerImage();
+    }
+
+    public void OnMouseExit()
+    {
+        Debug.Log("Mouse leave");
+        SetInnactiveMarkerImage();
+    }
+    #endregion
+
+    #region Private Functions 
+    private void SetActiveMarkerImage() {
+        GetComponent<Image>().sprite = Resources.Load<Sprite>("Notification/location-pointer-yellow");
+    }
+
+    private void SetInnactiveMarkerImage()
+    {
+        GetComponent<Image>().sprite = Resources.Load<Sprite>("Notification/location-pointer-purple");
+    }
+
+    #endregion
+}
