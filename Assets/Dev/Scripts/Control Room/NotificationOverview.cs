a8e425b3222b1567dbc768f2f0a62a4f77497341
jordy1111100 <jordyberg@gmail.com>
2019-04-30 16:03:31 +0200

    Created emty script and new display for overview notifications

---
+++ Assets/Dev/Scripts/Control Room/NotificationOverview.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationOverview : MonoBehaviour {
+
+    //The purpose of this class is to display the contents of a MainNotification when recieved.
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	
+}
