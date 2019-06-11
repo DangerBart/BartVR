dc082974802596a2ffaa4b756ea9da3a27b09862
Tim Meermans <timmeermans@live.nl>
2019-06-11 13:45:25 +0200

    Added script for detecting controllers, result reflected on canvas in world space

---
+++ Assets/Dev/Scripts/VR/OVRinputTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OVRinputTest : MonoBehaviour {
+    private string[] controllers;
+
+    [SerializeField]
+    private Text text;
+    [SerializeField]
+    private Text length;
+
+    // Use this for initialization
+    void Start () {
+        controllers = Input.GetJoystickNames();
+
+        foreach (string controller in controllers) {
+            text.text += controller + "\n";
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        length.text = controllers.Length.ToString();
+    }
+}
